cda22226f3df6b0faa15478bea190cd045a812de
GustavoTYX <130181989+GustavoTYX@users.noreply.github.com>
2023-08-13 01:14:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const keyElement = document.getElementById("generatedKey");
+const timerElement = document.getElementById("timer");
 
-let key = generateRandomKey();
+let key = "";
+let timeLeft = 24 * 60 * 60; // 24 hours in seconds
 
 function generateRandomKey() {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -11,4 +13,26 @@ function generateRandomKey() {
     return result;
 }
 
-keyElement.textContent = key;
+function updateTimer() {
+    const hours = Math.floor(timeLeft / 3600);
+    const minutes = Math.floor((timeLeft % 3600) / 60);
+    const seconds = timeLeft % 60;
+    timerElement.textContent = `${hours}h ${minutes}m ${seconds}s`;
+}
+
+function updateKeyAndTimer() {
+    key = generateRandomKey();
+    keyElement.textContent = key;
+    timeLeft = 24 * 60 * 60;
+    updateTimer();
+}
+
+function checkIPAndGenerateKey() {
+    // Aqui você pode adicionar a lógica para verificar o IP se necessário
+    // No exemplo abaixo, estamos apenas gerando uma nova chave a cada 24 horas
+    updateKeyAndTimer();
+}
+
+updateKeyAndTimer(); // Gere uma chave e configure o timer ao carregar a página
+
+setInterval(checkIPAndGenerateKey, 1000); // Verifique o IP e gere nova chave a cada segundo
