c4da1642db00331a81ddec7f72c2f7883b911f45
GustavoTYX <130181989+GustavoTYX@users.noreply.github.com>
2023-08-13 01:09:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const keyElement = document.getElementById("generatedKey");
-const resetButton = document.getElementById("resetButton");
-const timerElement = document.getElementById("timer");
 
 let key = generateRandomKey();
-let timeLeft = 24 * 60 * 60; // 24 hours in seconds
 
 function generateRandomKey() {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -14,50 +11,22 @@ function generateRandomKey() {
     return result;
 }
 
-function updateTimer() {
-    const hours = Math.floor(timeLeft / 3600);
-    const minutes = Math.floor((timeLeft % 3600) / 60);
-    const seconds = timeLeft % 60;
-    timerElement.textContent = `${hours}h ${minutes}m ${seconds}s`;
-}
-
-resetButton.addEventListener("click", () => {
+function updateKey() {
     key = generateRandomKey();
     keyElement.textContent = key;
-    sendKeyToOtherSite(key);
-});
-
-function update() {
-    if (timeLeft > 0) {
-        timeLeft--;
-        updateTimer();
-    } else {
-        key = generateRandomKey();
-        updateTimer();
-        timeLeft = 24 * 60 * 60;
-    }
+    sendKeyToServer(key);
 }
 
-function sendKeyToOtherSite(key) {
-    // Fazer uma requisição para enviar a chave para o site vazio
-    // Aqui você usaria uma abordagem como XMLHttpRequest, fetch ou outras bibliotecas de requisição
-    // No exemplo abaixo, usaremos o método fetch
-
-    const url = "URL_DO_SEU_SITE_VAZIO/key_receiver.php";
-    fetch(url, {
+function sendKeyToServer(key) {
+    // Send the key to the PHP server using AJAX or fetch
+    fetch("save_key.php", {
         method: "POST",
-        body: JSON.stringify({ key: key }),
-        headers: {
-            "Content-Type": "application/json"
-        }
+        body: JSON.stringify({ key: key })
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Key sent to the other site:", data);
-    })
-    .catch(error => {
-        console.error("Error sending key:", error);
-    });
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error sending key:", error));
 }
 
-setInterval(update, 1000);
+// Atualize a chave ao carregar a página
+updateKey();
